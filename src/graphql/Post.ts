d2cb96236de0af5dcc081190d1c279a2674eeb63
Bence Rozsa <robence@gmail.com>
2020-11-28 13:34:58 +0100

    feat(schema): split schema into modules

---
+++ src/graphql/Post.ts
@@ -0,0 +1,89 @@
+import { objectType, extendType, stringArg, intArg } from '@nexus/schema'
+
+export const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id()
+    t.model.title()
+    t.model.content()
+    t.model.published()
+    t.model.author()
+    t.model.authorId()
+  },
+})
+
+export const PostQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.post()
+
+    t.list.field('feed', {
+      type: 'Post',
+      resolve: (_, args, ctx) => {
+        return ctx.prisma.post.findMany({
+          where: { published: true },
+        })
+      },
+    })
+
+    t.list.field('filterPosts', {
+      type: 'Post',
+      args: {
+        searchString: stringArg({ nullable: true }),
+      },
+      resolve: (_, { searchString }, ctx) => {
+        return ctx.prisma.post.findMany({
+          where: {
+            OR: [
+              { title: { contains: searchString } },
+              { content: { contains: searchString } },
+            ],
+          },
+        })
+      },
+    })
+  },
+})
+
+export const PostMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.crud.createOneUser({ alias: 'signupUser' })
+    t.crud.deleteOnePost()
+
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: stringArg({ nullable: false }),
+        content: stringArg(),
+        authorEmail: stringArg({ nullable: false }),
+      },
+      resolve: (_, { title, content, authorEmail }, ctx) => {
+        return ctx.prisma.post.create({
+          data: {
+            title,
+            content,
+            published: false,
+            author: {
+              connect: { email: authorEmail },
+            },
+          },
+        })
+      },
+    })
+
+    t.field('publish', {
+      type: 'Post',
+      nullable: true,
+      args: {
+        id: intArg(),
+      },
+      resolve: (_, { id }, ctx) => {
+        return ctx.prisma.post.update({
+          where: { id: Number(id) },
+          data: { published: true },
+        })
+      },
+    })
+  },
+})
